c4f69913953a00dfaea719d681d8732471d29e8d
son bui <sonvbvnvn@gmail.com>
2022-06-18 09:02:52 +0700

    setting rules

--- addons_1/internship_manager/wizards/batch_update_student.py
+++ addons_1/internship_manager/wizards/batch_update_student.py
@@ -16,12 +16,21 @@ class BatchUpdateStudentWizard(models.TransientModel):
     class_id = fields.Many2one('classes', string='class', default=False)
     address = fields.Char(string='address')
     student_image = fields.Binary(string='student_image')
+    student_country = fields.Many2one(
+        comodel_name='res.country', string='country', store=True, default=241)
+
+    student_state = fields.Many2one(
+        'res.country.state', string="State", store=True)
 
     def multi_update(self):
         ids = self.env.context['active_ids']  # selected record ids
         students = self.env["students"].browse(ids)
         new_data = {}
 
+        if self.student_country:
+            new_data["student_country"] = self.student_country
+        if self.student_state:
+            new_data["student_state"] = self.student_state
         if self.student_image:
             new_data["student_image"] = self.student_image
         if self.address:
